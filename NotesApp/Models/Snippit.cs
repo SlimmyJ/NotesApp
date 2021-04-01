e4b100119985ba1093cf805dd0e80187388cce2a
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-04-01 13:30:50 +0200

    test

---
+++ NotesApp/Models/Snippit.cs
@@ -0,0 +1,29 @@
+﻿namespace NotesApp.Models
+    {
+    internal class Snippit
+        {
+        public Snippit()
+            {
+            }
+
+        public Snippit(string name, string summary, string content)
+            {
+            Name = name;
+            Summary = summary;
+            Content = content;
+            Author = "Empty author";
+            }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public string Summary { get; set; }
+
+        public override string ToString()
+            {
+            return $"{Name} | {Summary}";
+            }
+        }
+    }
---
+++ NotesApp/Views/AddSnippitPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+using NotesApp.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotesApp.Views
+    {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddSnippitPage : ContentPage
+        {
+        public AddSnippitPage()
+            {
+            InitializeComponent();
+            BindingContext = new Snippit();
+            }
+
+        private void LoadSnippit(string snippitName)
+            {
+            var snippit = new Snippit
+                {
+                Name = snippitName
+                };
+
+            BindingContext = snippit;
+            }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+            {
+            var snippit = new Snippit();
+
+            if (string.IsNullOrWhiteSpace(snippit.Name))
+                //Check if file already exists
+                if (string.IsNullOrWhiteSpace(snippit.Name))
+                    {
+                    string _fileName = Path.Combine(Environment.GetFolderPath(), snippit);
+                    }
+            }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+            {
+            }
+        }
+    }
